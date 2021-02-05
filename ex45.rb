abaac30553bbd901b67a2622fa2f1430bc986f02
aliyuksekkayabrandeis <ayuksekkaya@brandeis.edu>
2021-02-05 14:35:53 -0500

    trying git

---
+++ ex45.rb
@@ -0,0 +1,36 @@
+class Scene
+    def enter()
+    end
+end
+
+class Engine
+    def initialize(scene_map)
+    end
+
+    def play()
+    end
+
+end
+
+
+class Sherman < Scene
+    def enter()
+
+    end
+end
+
+class East < Scene
+    def enter()
+
+    end
+end
+
+
+class Road < Scene
+    def enter()
+
+    end
+end
+
+
+
